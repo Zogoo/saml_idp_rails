31e2bf4b89dcd0fa470d91c50607f1208eaf3897
zogoo <ch.tsogbadrakh@gmail.com>
2025-02-16 16:41:09 +0100

    Remove double namespace from path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SamlIdpRails::Engine.routes.draw do
-  get "saml_idp/:uuid/metadata", to: "saml_idp#metadata", as: "metadata"
-  post "saml_idp/:uuid/sso", to: "saml_idp#sso_request", as: "sso_post"
-  get "saml_idp/:uuid/sso", to: "saml_idp#sso_request", as: "sso_redirect"
-  post "saml_idp/:uuid/logout", to: "saml_idp#slo_request", as: "slo_post"
-  get "saml_idp/:uuid/logout", to: "saml_idp#slo_request", as: "slo_redirect"
-  post "saml_idp/:uuid/slo_request", to: "saml_idp#initiate_slo", as: "initiate_slo"
-  get "saml_idp/:uuid/attribute", to: "saml_idp#attribute", as: "attribute"
+  get ":uuid/metadata", to: "saml_idp#metadata", as: "metadata"
+  post ":uuid/sso", to: "saml_idp#sso_request", as: "sso_post"
+  get ":uuid/sso", to: "saml_idp#sso_request", as: "sso_redirect"
+  post ":uuid/logout", to: "saml_idp#slo_request", as: "slo_post"
+  get ":uuid/logout", to: "saml_idp#slo_request", as: "slo_redirect"
+  post ":uuid/slo_request", to: "saml_idp#initiate_slo", as: "initiate_slo"
+  get ":uuid/attribute", to: "saml_idp#attribute", as: "attribute"
 end
